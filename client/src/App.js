e28d88df7d3ce4ec20900bba6449e197baad3acf
Onidredd <joshuaf@missionreadyhq.com>
2023-03-12 17:43:18 +1300

    Completed dockerization and created running containers with docker compose

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import styles from './App.module.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+  const [test, setTest] = useState([])
+
+  const fetchTest = async () => {
+   try{
+    const response = await axios.get('http://localhost:8000/test')
+    console.log('Response from server:', response.data)
+    setTest(response.data)
+    console.log('Test in state:', test);
+  }
+  catch (error){
+    console.error(error)
+  }};
+
+  useEffect(
+    () => {
+      fetchTest()
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    return (
+    <div className={styles.App}>
+      <header className={styles.App_header}>
+        {test.map(
+          test =>
+            <h1 key={test}>{test.message}</h1>
+        )}
       </header>
     </div>
   );
--- server/server.js
+++ server/server.js
@@ -2,12 +2,15 @@ const express = require('express')
 const dotenv = require('dotenv').config()
 const colors = require('colors')
 const connectDB = require('./Config/db')
+const cors = require('cors')
 
 const port = process.env.PORT || 8000
 
 connectDB()
 
 const app = express()
+
+app.use(cors())
 
 app.use(express.json())
 app.use(express.urlencoded({ extended:false }))
